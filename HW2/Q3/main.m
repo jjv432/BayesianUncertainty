c0f7cb1013f597b67fc20b944c952136dd4b5b42
jjv432 <jackvranicar43@gmail.com>
2025-09-28 14:06:23 -0400

    Part 1 still works

--- HW2/Q3/main.m
+++ HW2/Q3/main.m
@@ -1,39 +1,80 @@
-clc; clear; close all
+clc; clearvars -except P psi; close all
 
 load energy_data.mat
-
-figure()
-plot(P, psi, 'DisplayName', 'Data')
-legend()
+addpath("./mcmcstat/")
 
 %% Part 1
-numVars = 4;
-LB = [0 0 0 0];
-UB = [10, 10, 10, 10];
-costFunctionHandle = @(theta, numVars) CostFunction(theta, P, psi);
 
+%{
+Setting up a particle swarm optimization to guess the coefficients the
+model should use
+%}
+
+numVars = 4; % order of the model
+
+% Bounds that the particles can guesss between
+LB = -20 * ones(numVars,1);
+UB = 20* ones(numVars, 1);
+
+% Cost function used by the particle swarm
+data.xdata = P;
+data.ydata = psi;
+costFunctionHandle = @(theta, numVars) CostFunction(theta,data);
+
+% Run particle swarm
 optimalTheta = particleswarm(costFunctionHandle, numVars, LB, UB);
 
-numParams = numel(optimalTheta);
+% Create y vals based on the optimized coefficients
+modelPsi = myPolynomialFunc(data, optimalTheta);
 
-for i = 1:numParams
-    xVals(:, i) = P.^i;
-end
+% Plot
+figure()
+hold on
+plot(P, psi, 'DisplayName', 'Data')
+plot(P, modelPsi, 'DisplayName', 'Model')
+legend()
 
-simulatedY = xVals * optimalTheta';
+%% Part 2
+% ssfun = @CostFunction;
+% model.ssfun=ssfun;
+% 
+% % Chains
+% 
+% model.sigma2 = 1e-4;      %initial guess on variance
+% model.S20 = model.sigma2; %prior for sigma2
+% model.N0  = 1;            %prior accuracy for S20
+% options.updatesigma = 1;  %update variance as part of the inference
+% options.method = 'dram';  %this applies the DRAM algorithm
+% model.N  = length(data.xdata); %number of data points
+% 
+% options.nsimu = 10000; %number of iterations in the Metropolis method
+% [results, chain, s2chain]= mcmcrun(model,data,optimalTheta,options);
+% chainstats(chain,results) %print chain statistics
 
-plot(P, simulatedY);
+%% Functions
 
-function cost = CostFunction(theta, xData, yData)
-
-    xData = xData(:);
-    numParams = numel(theta);
+% This function takes in x values of interest and polynomial coefficients
+% and spits out corresponding y values
+function yVals = myPolynomialFunc(data, coeff)
+    if class(data) == "struct"
+        xVals =  data.xdata;
+    else
+        xVals = data;
+    end
+    numParams = numel(coeff);
 
     for i = 1:numParams
-        xVals(:, i) = xData.^i;
+        xMat(:, i) = xVals.^i;
     end
 
-    simulatedY = xVals * theta';
+    yVals = xMat * coeff';
+end
 
-    cost = sum(abs(yData' - simulatedY));
+% Taking the difference between model data and experimental data as the
+% cost
+function cost = CostFunction(theta, data)
+
+    simulatedY = myPolynomialFunc(data.xdata,theta);
+
+    cost = sum(abs(data.ydata' - simulatedY));
 end
