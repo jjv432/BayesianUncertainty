feb1dd94f29505a90bc634c5a9b2e1d1cf4cd434
jjv432 <jackvranicar43@gmail.com>
2025-10-01 08:36:58 -0400

    Generalized plotting, should make adding more linkages easier

--- Project/main.m
+++ Project/main.m
@@ -4,21 +4,18 @@ Need to add the extra bits at the end of each linkage
 %}
 L = 5;
 l = 1;
-link1 = linkage('r', L, l, 'r');
-link2 = linkage('l', L, l, 'k');
-
-%% Plotting
-% figure()
-% axis equal
-% axis padded
-% grid on
-% link.plotLinkage(pi/2)
+% link1 = linkage('r', L, l, 'r');
+% link2 = linkage('l', L, l, 'k');
+% link3 = linkage('r', L, l, 'c');
+links(1) = linkage('r', L, l, 'r');
+links(2) = linkage('l', L, l, 'k');
+links(3) = linkage('r', L, l, 'c');
 
 %% Animating
 xmin = -L-1;
 xmax = 1;
 ymin = -l-1;
-ymax = L+1;
+ymax = 2*L;
 
 figure()
 hold on
@@ -29,14 +26,22 @@ axis([xmin, xmax, ymin, ymax]);
 
 offset = pi/20;
 thetas = linspace(pi/2 - offset, pi/2 + offset, 250);
-
+linkageOffset = 0;
 for i = 1:numel(thetas)
-    link1 = link1.plotLinkage(thetas(i));
-    link2.basePosition = link1.endPosition;
-    link2 = link2.plotLinkage(pi-thetas(i));
+    links(1) = links(1).plotLinkage(thetas(i));
+    for j = 2:numel(links)
+        links(j).basePosition = links(j-1).endPosition;
+        if mod(j,2) == 0
+            links(j) = links(j).plotLinkage(pi-thetas(i));
+        else
+            links(j) = links(j).plotLinkage(thetas(i));
+        end
+    end
+
     pause(.001);
-    delete(link1.figs)
-    delete(link2.figs)
+    for k = 1:numel(links)
+        delete(links(k).figs);
+    end
 end
 
 
